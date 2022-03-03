28fe1fceba2778a96ab83548d087707c813bb45c
koobawuah <me@bawuahboakye.com>
2022-03-03 22:05:55 +0000

    Made changes to App js to receive the data from the reduxjs toolkit store using useSelector hook from react-redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+    const products = useSelector( (state) => state.products )
+
   return (
     <div className="App">
       <p> Testshoppingcart </p>
+      <br/>
+      <b> Cart </b>
+      <br/>
+      <p> Products </p>
+      { products.map( (item, index) => ( <div key={index}> <p> {item.name} </p> <b> GHS{item.price}</b> </div> ) ) }
     </div>
   );
 }
